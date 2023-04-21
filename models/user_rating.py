c5b2cc88d9d106f72954a324cc90d3a9a4936800
CANSU GÜREL <cansugurel@eskisehir.edu.tr>
2023-04-21 22:42:05 +0300

    Rating updated. to_dict function added. Rate and get rating routes added.

--- models/user_rating.py
+++ models/user_rating.py
@@ -1,5 +1,5 @@
-from mongoengine import Document, CASCADE, ReferenceField, StringField
-
+from mongoengine import Document, CASCADE, ReferenceField, StringField, DateTimeField
+from datetime import datetime
 from .user import User
 from .service import Service
 
@@ -7,3 +7,12 @@ class UserRating(Document):
     user = ReferenceField(User, reverse_delete_rule=CASCADE)
     service = ReferenceField(Service, reverse_delete_rule=CASCADE)
     rating = StringField(required=True, choices=['1', '2', '3', '4', '5'])
+    rated_at = DateTimeField(default=datetime.utcnow)
+    
+    def to_dict(self):
+        return {
+            'user_full_name': str(self.user.full_name),
+            'service_name': str(self.service.description),
+            'rating': self.rating,
+            'rated_at': self.rated_at.isoformat()
+        }
--- routes/service.py
+++ routes/service.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, jsonify, request, current_app
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from mongoengine.errors import ValidationError
 from models.service import Service
 from flask_security import roles_required, login_required
 from decorators import api_login_required
+from mongoengine.errors import DoesNotExist
 
 from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
 import pickle
 import redis
+
+from models.user import User
+from models.user_rating import UserRating
 
 
 service_bp = Blueprint('service', __name__, url_prefix='/services')
@@ -15,8 +19,6 @@ service_bp = Blueprint('service', __name__, url_prefix='/services')
 redis_client = redis.Redis(host='localhost', port=6379)
 
 @service_bp.route('/service', methods=['GET'])
-@api_login_required
-@jwt_required()
 def get_services():
     services = Service.objects.all()
     return jsonify([s.to_dict() for s in services]), 200
@@ -36,6 +38,7 @@ def get_summarizer():
 
 
 @service_bp.route('/summarizer', methods=['POST'])
+@api_login_required
 def create_service():
 
     #get information of models
@@ -60,3 +63,37 @@ def create_service():
         return jsonify(service.to_dict()), 201
     except ValidationError as e:
         return jsonify({'error': str(e)}), 400
+
+
+@service_bp.route("/rating", methods=["POST"])
+@api_login_required
+@jwt_required()
+def rate_service():
+    current_user_id = get_jwt_identity()
+
+    user = User.objects(id=current_user_id).first()
+    if not user:
+        return {"error": "User not found"}, 404
+
+    service_name = request.json.get("service_name")
+    rating = request.json.get("rating")
+
+    service = Service.objects(name=service_name).first()
+    if not service:
+        return {"error": "Service not found"}, 404
+
+    try:
+        user_rating = UserRating.objects.get(user=user, service=service)
+        user_rating.rating = rating
+    except DoesNotExist:
+        user_rating = UserRating(user=user, service=service, rating=rating)
+
+    user_rating.save()
+
+    return {"message": "Rating saved"}, 201
+
+
+@service_bp.route('/ratings', methods=['GET'])
+def get_ratings():
+    ratings = UserRating.objects.all()
+    return jsonify([s.to_dict() for s in ratings]), 200
